8a80a8869e7e8fd6a6877b5793d4b1dffe301708
Thien-Nam <thiennam.tnd@gmail.com>
2019-05-18 16:34:51 -0600

    add notifications page

---
+++ application/src/Pages/Notifications/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+function Notifications(props) {
+    return (
+	<Grid container direction="column" justify="center" alignItems="center">
+	  <p> Notifications </p>
+	</Grid>
+    );
+};
+
+export default Notifications;
