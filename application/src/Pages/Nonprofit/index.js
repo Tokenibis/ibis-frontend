ab33181fec319910099817acdfe21766530c8428
Thien-Nam <thiennam.tnd@gmail.com>
2019-05-29 23:45:45 -0600

    partial nonprofit page

--- application/src/Pages/Nonprofit/index.js
+++ application/src/Pages/Nonprofit/index.js
@@ -1,33 +1,180 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 import Grid from '@material-ui/core/Grid';
 
 import NewsList from '../NewsList';
 import EventList from '../EventList';
-import DonationList from '../DonationList';
+import ListView from '../__Common__/ListView';
+import DonationSublist from '../DonationList/sublist';
 
-function Nonprofit({ handleWindow }) {
-    return (
-	<Grid container direction="column" justify="center" alignItems="center">
-	  <p> Nonprofit </p>
-	  <div style={{ color: "#b0bf25" }}
-	      onClick={(e) => handleWindow(<NewsList />)}>
-	    News List
-	  </div>
-	  <div style={{ color: "#b0bf25" }}
-	      onClick={(e) => handleWindow(<EventList />)}>
-	    Events List
-	  </div>
-	  <div style={{ color: "#b0bf25" }}
-	      onClick={(e) => handleWindow(<DonationList />)}>
-	    Donations List 
-	  </div>
-	</Grid>
-    );
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import LikeIcon from '@material-ui/icons/FavoriteBorder';
+import BookmarkIcon from '@material-ui/icons/BookmarkBorder';
+import NonprofitIcon from '@material-ui/icons/CardGiftcard';
+import ScrollToTop from "react-scroll-up";
+import UpIcon from '@material-ui/icons/ArrowUpward';
+import Fab from '@material-ui/core/Fab';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const styles = theme => ({
+    root: {
+	width: '100%',
+    },
+    avatar: {
+	borderStyle: 'solid',
+	borderWidth: '2px',
+	borderColor: theme.palette.secondary.main,
+    },
+    title: {
+	color: theme.palette.primary.main,
+	fontWeight: 'bold',
+    },
+    subheader: {
+	color: theme.palette.tertiary.main,
+    },
+    nonprofitIcon: {
+	fontSize: 18,
+	marginBottom: -4,
+	marginRight: theme.spacing.unit,
+    },
+    description: {
+	color: theme.palette.tertiary.main,
+    },
+    progress: {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+        transform: 'translate(-50%, -50%)'
+    },
+    card: {
+	width: '100%',
+	marginBottom: theme.spacing.unit,
+    },
+    media: {
+	height: 160,
+    },
+    readMore: {
+	marginLeft: 'auto',
+	marginRight: theme.spacing.unit * 2,
+	color: theme.palette.secondary.main,
+	fontWeight: 'bold',
+	float: 'right',
+    }
+})
+
+class Nonprofit extends Component {
+    
+    createPage(nonprofit) {
+	let { classes } = this.props
+	return (
+	    <div className={classes.root}>
+	      <Card raised className={classes.card}>
+		<CardMedia
+		    className={classes.media}
+  		    image={require(`../../Static/Images/egypt/pic1.jpg`)}
+		/>
+		<CardContent>
+		  {nonprofit.description}
+		  <Typography variant="body2" className={classes.description}>
+		  </Typography>
+		  <Typography variant="body2" className={classes.description}>
+		    https://www.trevornoahfoundation.org
+		  </Typography>
+		</CardContent>
+		<CardActions>
+		  <IconButton color="secondary" aria-label="Like">
+		    <LikeIcon />
+		  </IconButton>
+		</CardActions>
+	      </Card>
+	      <Typography
+		  variant="h6"
+	      >
+		Donations
+	      </Typography>
+	      <DonationSublist data={(nonprofit.user.transactionSet)} />
+	    </div>
+	); 
+    }
+    
+    render() {
+	let { classes, id } = this.props
+
+	const query = gql`
+	    query {
+		nonprofit(id: "${id}") {
+		    description
+		    title
+		    user {
+			followerCount
+			transactionSet {
+			    edges {
+				node {
+				    id
+				    amount
+				    description
+				    created
+				    user {
+					id
+					firstName
+					lastName
+				    }
+				    target {
+					id
+					firstName
+					lastName
+					nonprofit {
+					    id 
+					    title
+					}
+				    }
+				}
+			    }
+			}
+			postSet {
+			    edges {
+				node {
+				    id
+				    event {
+					id
+					title
+				    }
+				    article {
+					id
+					title
+				    }
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	`;
+
+	return (
+	    <Query query={query}>
+	      {({ loading, error, data }) => {
+		  if (loading) return <CircularProgress className={classes.progress} />;
+		  if (error) return `Error! ${error.message}`;
+		  return this.createPage(data.nonprofit);
+	      }}
+	    </Query>
+	);
+    };
 };
 
 Nonprofit.propTypes = {
-    handleWindow: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
 };
 
-export default Nonprofit;
+export default withStyles(styles)(Nonprofit);
