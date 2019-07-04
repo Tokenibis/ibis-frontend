74ecdad3f8fcda4c908f76f77df162a9baaa64eb
Thien-Nam <thiennam.tnd@gmail.com>
2019-07-04 00:49:27 -0600

    enable correct csrf token delivery

--- application/src/App.js
+++ application/src/App.js
@@ -11,7 +11,13 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: 'http://api.tokenibis.org:8000/graphql/' }),
+    link: new HttpLink({
+	uri: 'http://api.tokenibis.org/graphql/',
+	credentials: 'include',
+	headers: {
+	    'X-CSRFToken': document.cookie.match('(^|;) ?csrftoken=([^;]*)(;|$)')[2],
+	},
+    }),
     cache: new InMemoryCache(),
 });
 
