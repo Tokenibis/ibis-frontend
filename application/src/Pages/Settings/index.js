7f1570f318344e9527e72a023bfb5caf925def77
Thien-Nam <thiennam.tnd@gmail.com>
2019-11-28 12:49:33 -0700

    change settings page to sync with backend api

--- application/src/Pages/Settings/index.js
+++ application/src/Pages/Settings/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import LinearProgress from '@material-ui/core/LinearProgress';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -34,184 +37,54 @@ const styles = theme => ({
     },
 });
 
+const SETTINGS = {
+    notifyEmailFollow: 'Boolean',
+    notifyEmailTransaction: 'Boolean',
+    notifyEmailLike: 'Boolean',
+    visibilityFollow: 'String',
+    visibilityDonation: 'String',
+    visibilityTransaction: 'String',
+}
+
+const QUERY = gql`
+    query Settings($id: ID!) {
+	person(id: $id) {
+	    ${Object.keys(SETTINGS).join('\n')}
+	}
+    }
+`;
+
+const MUTATION = gql`
+    mutation UpdateSettings($id: ID! ${Object.keys(SETTINGS).map(k => '$' + k + ': ' + SETTINGS[k]).join(' ')}) {
+	updatePerson(id: $id ${Object.keys(SETTINGS).map(k => k + ': $' + k).join(' ')}) {
+	    person {
+		id
+		notifyEmailFollow
+	    }
+	}
+    }
+`;
+
 class Settings extends Component {
 
-    state = {
-	settings: {
-	    transaction: 0,
-	    follow: 0,
-	    pushTransaction: true,
-	    pushFollow: true,
-	    pushLike: true,
-	    emailTransaction: true,
-	    emailFollow: false,
-	    emailLike: false,
-	},
-    };
+    updateSetting = (mutation, refetch, key, value) => {
 
-    handleInput = (key, value) => () => {
-	let { settings } = this.state;
-	settings[key] = value;
-	this.setState(settings);
-    };
+	mutation({ variables: {
+	    [key]: value,
+	}}).then(response => {
+	    refetch();
+	}).catch(error => {
+	    console.log(error);
+	    console.log(error.response);
+	});
+    }
 
-    render() {
+    inner(data, refetch, mutation) {
 
-	let { settings } = this.state;
-	let { classes } = this.props;
-	
+	let { classes, context } = this.props;
+
 	return (
 	    <div>
-	      <List
-		  subheader={
-		      <ListSubheader disableSticky className={classes.subheader}>
-			Following Privacy
-		      </ListSubheader>
-		  }
-		  className={classes.root}
-	      >
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <PublicIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Public" />
-		  <ListItemSecondaryAction>
-		    <Radio
-			onChange={this.handleInput('follow', 0)}
-			checked={settings.follow === 0}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <FollowingIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Following Only" />
-		  <ListItemSecondaryAction>
-		    <Radio
-			onChange={this.handleInput('follow', 1)}
-			checked={settings.follow === 1}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <MeIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Me Only" />
-		  <ListItemSecondaryAction>
-		    <Radio
-			onChange={this.handleInput('follow', 2)}
-			checked={settings.follow === 2}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-	      </List>
-	      <List
-		  subheader={
-		      <ListSubheader disableSticky className={classes.subheader}>
-			Transaction Privacy
-		      </ListSubheader>
-		  }
-		  className={classes.root}
-	      >
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <PublicIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Public" />
-		  <ListItemSecondaryAction>
-		    <Radio
-			onChange={this.handleInput('transaction', 0)}
-			checked={settings.transaction === 0}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <FollowingIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Following Only" />
-		  <ListItemSecondaryAction>
-		    <Radio
-			onChange={this.handleInput('transaction', 1)}
-			checked={settings.transaction === 1}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <MeIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Me Only" />
-		  <ListItemSecondaryAction>
-		    <Radio
-			onChange={this.handleInput('transaction', 2)}
-			checked={settings.transaction === 2}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-	      </List>
-	      <List
-		  subheader={
-		      <ListSubheader disableSticky className={classes.subheader}>
-			Push Notifications
-		      </ListSubheader>
-		  }
-		  className={classes.root}
-	      >
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <TransactionIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Transactions" />
-		  <ListItemSecondaryAction>
-		    <Switch
-		    edge="end"
-		    onChange={this.handleInput('pushTransaction', !settings.pushTransaction)}
-		    checked={settings.pushTransaction}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <FollowIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Follows" />
-		  <ListItemSecondaryAction>
-		    <Switch
-		    edge="end"
-		    onChange={this.handleInput('pushFollow', !settings.pushFollow)}
-		    checked={settings.pushFollow}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-		<ListItem>
-		  <ListItemIcon>
-		    <LikeIcon />
-		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Likes" />
-		  <ListItemSecondaryAction>
-		    <Switch
-		    edge="end"
-		    onChange={this.handleInput('pushLike', !settings.pushLike)}
-		    checked={settings.pushLike}
-		    />
-		  </ListItemSecondaryAction>
-		</ListItem>
-		<CustomDivider />
-	      </List>
 	      <List
 		  subheader={
 		      <ListSubheader disableSticky className={classes.subheader}>
@@ -223,28 +96,38 @@ class Settings extends Component {
 		<CustomDivider />
 		<ListItem>
 		  <ListItemIcon>
-		    <TransactionIcon />
+		    <FollowIcon />
 		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Transactions" />
+		  <ListItemText className={classes.text} primary="Follows" />
 		  <ListItemSecondaryAction>
 		    <Switch
-		    edge="end"
-		    onChange={this.handleInput('emailTransaction', !settings.emailTransaction)}
-		    checked={settings.emailTransaction}
+			edge="end"
+			checked={data.person.notifyEmailFollow}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'notifyEmailFollow',
+			    !data.person.notifyEmailFollow,
+			))}
 		    />
 		  </ListItemSecondaryAction>
 		</ListItem>
 		<CustomDivider />
 		<ListItem>
 		  <ListItemIcon>
-		    <FollowIcon />
+		    <TransactionIcon />
 		  </ListItemIcon>
-		  <ListItemText className={classes.text} primary="Follows" />
+		  <ListItemText className={classes.text} primary="Transactions" />
 		  <ListItemSecondaryAction>
 		    <Switch
-		    edge="end"
-		    onChange={this.handleInput('emailFollow', !settings.emailFollow)}
-		    checked={settings.emailFollow}
+			edge="end"
+			checked={data.person.notifyEmailTransaction}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'notifyEmailTransaction',
+			    !data.person.notifyEmailTransaction,
+			))}
 		    />
 		  </ListItemSecondaryAction>
 		</ListItem>
@@ -256,15 +139,237 @@ class Settings extends Component {
 		  <ListItemText className={classes.text} primary="Likes" />
 		  <ListItemSecondaryAction>
 		    <Switch
-		    edge="end"
-		    onChange={this.handleInput('emailLike', !settings.emailLike)}
-		    checked={settings.emailLike}
+			edge="end"
+			checked={data.person.notifyEmailLike}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'notifyEmailLike',
+			    !data.person.notifyEmailLike,
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+	      </List>
+	      <List
+		  subheader={
+		      <ListSubheader disableSticky className={classes.subheader}>
+			Following Visibility
+		      </ListSubheader>
+		  }
+		  className={classes.root}
+	      >
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <PublicIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Public" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityFollow === 'PC'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityFollow',
+			    'PC',
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <FollowingIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Following Only" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityFollow === 'FL'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityFollow',
+			    'FL',
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <MeIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Me Only" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityFollow === 'PR'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityFollow',
+			    'PR',
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+	      </List>
+	      <List
+		  subheader={
+		      <ListSubheader disableSticky className={classes.subheader}>
+			Donation Visibility
+		      </ListSubheader>
+		  }
+		  className={classes.root}
+	      >
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <PublicIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Public" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityDonation === 'PC'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityDonation',
+			    'PC',
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <FollowingIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Following Only" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityDonation === 'FL'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityDonation',
+			    'FL',
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <MeIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Me Only" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityDonation === 'PR'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityDonation',
+			    'PR',
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+	      </List>
+	      <List
+		  subheader={
+		      <ListSubheader disableSticky className={classes.subheader}>
+			Transaction Visibility
+		      </ListSubheader>
+		  }
+		  className={classes.root}
+	      >
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <PublicIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Public" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityTransaction === 'PC'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityTransaction',
+			    'PC',
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <FollowingIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Following Only" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityTransaction === 'FL'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityTransaction',
+			    'FL',
+			))}
+		    />
+		  </ListItemSecondaryAction>
+		</ListItem>
+		<CustomDivider />
+		<ListItem>
+		  <ListItemIcon>
+		    <MeIcon />
+		  </ListItemIcon>
+		  <ListItemText className={classes.text} primary="Me Only" />
+		  <ListItemSecondaryAction>
+		    <Radio
+			checked={data.person.visibilityTransaction === 'PR'}
+			onChange={() => (this.updateSetting(
+			    mutation,
+			    refetch,
+			    'visibilityTransaction',
+			    'PR',
+			))}
 		    />
 		  </ListItemSecondaryAction>
 		</ListItem>
 		<CustomDivider />
 	      </List>
 	    </div>
+	);
+    };
+
+    render() {
+
+	let { classes, context } = this.props;
+
+	return (
+	    <Query
+	      fetchPolicy="no-cache"
+	      query={QUERY}
+	      variables={{ id: context.userID }}
+	    >
+	      {({ loading, error, data, refetch }) => {
+		  if (loading) return <LinearProgress className={classes.progress} />;
+		  if (error) return `Error! ${error.message}`;
+		  return (
+		      <Mutation mutation={MUTATION} variables={{ id: context.userID}}>
+			{mutation => (
+			   this.inner(data, refetch, mutation)
+			)}
+		      </Mutation>
+		  );
+	      }}
+	    </Query>
 	);
     };
 };
