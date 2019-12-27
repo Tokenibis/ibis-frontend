12ac2c800afed45cfdcd7883340f7347fb72ade0
Thien-Nam <thiennam.tnd@gmail.com>
2019-12-26 20:08:51 -0700

    refactored dev/prod config file workflow and add deployment script

--- application/deploy.sh
+++ application/deploy.sh
@@ -17,16 +17,36 @@ if [ $# -ne 2 ]; then
    exit 1
 fi
 
+if [ ! $DIR/src/config.json ]; then
+    echo 'ERROR: src/config.json already exists. Aborting'
+    exit 1
+fi
+
+if [ ! -f $DIR/config_dev.json ]; then
+    echo 'ERROR: missing config_dev.json file'
+    exit 1
+fi
+
+if [ ! -f $DIR/config_prod.json ]; then
+    echo 'ERROR: missing config_prod.json file'
+    exit 1
+fi
+
 if [ $1 == '--production' ] || [ $1 == '-p' ]; then
     echo 'STOP! You are about deploy to production. Are you sure you want to continue? (y/N)'
     read response
     if [ $response != 'y' ]; then
        echo 'Aborting deployment'
        exit 0
+    else
+	cp $DIR/config_prod.json $DIR/src/config.json
     fi
-elif [ $1 != '--development' ] && [ $1 != '-d' ]; then
+elif [ $1  == '--development' ] || [ $1 == '-d' ]; then
+    cp $DIR/config_dev.json $DIR/src/config.json
+else
     echo 'ERROR: unrecognized arguments'
     exit 1
 fi
-    
+
 yarn build && ssh $2 rm /srv/app/build -rf && scp -r build/ $2:/srv/app/build
+rm $DIR/src/config.json
